10e5330d165662cc27e6d89eb7bd1b83401963b8
BBranko93 <bane_b80@outlook.com>
2021-02-28 20:11:59 -0700

    Add project files.

---
+++ EncapsulationPaintballGun/PaintballGun.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncapsulationPaintballGun
+{
+    class PaintballGun
+    {
+        public const int MAGAZINE_SIZE = 16;
+
+        private int paintBalls = 0;
+        private int paintBallsLoaded = 0;
+
+        /* When the game needs to display the number of balls left 
+         * and the number of balls loaded in the UI, 
+         * it can call GetPaintBalls and GetPaintBallsLoaded methods.*/
+        public int GetPaintBallsLoaded() { return paintBallsLoaded; }
+
+        public bool IsEmpty() { return paintBallsLoaded == 0; }
+
+        /*public int GetPaintBalls() { return paintBalls; }
+
+         * The game needs to able to set the number of balls.
+         * The SetPaintBalls method protects the balls field 
+         * by only allowing the game to set a positive number
+         * of balls. Then it calls Reload() to automatically reload the gun.
+        public void SetPaintBalls (int numberOfPaintBalls)
+        {
+            if (numberOfPaintBalls > 0)
+                paintBalls = numberOfPaintBalls;
+            Reload();
+        }*/
+
+        /* This is the declaration. It says that the name of the 
+         * property is PaintBalls, and its type is int.*/
+        public int PaintBalls
+        {
+            // The get accessor or getter is identical to the GetPaintBalls method
+            // that it replaced.
+            get { return paintBalls; }
+
+            /* The set accessor or setter is almost identical to the SetPaintBalls
+             * method. The only difference is it uses the value keyword where SetPaintBalls
+             * used its parameter. The value keyword will always contain the value
+             * being assigned by the set accessor.*/
+            set
+            {
+                if (value > 0)
+                    paintBalls = value;
+                Reload();
+            }
+        }
+
+        /* The only way to reload the gun is to call the Reload 
+         * method, which loads the gun with a full magazine,
+         * or the remaining number of balls if there isn't
+         * a full magazine's worth. This keeps the paintBalls and
+         * paintBallsLoaded from getting out of sync.*/
+        public void Reload()
+        {
+            Console.WriteLine("Reloading ...");
+            if (paintBalls > MAGAZINE_SIZE)
+                paintBallsLoaded = MAGAZINE_SIZE;
+            else
+                paintBallsLoaded = paintBalls;
+        }
+
+        /* The Shoot method returns true and decrements the balls
+         * field if the fun is loaded, or false if it isn't.*/
+        public bool Shoot()
+        {
+            if (paintBallsLoaded == 0) return false;
+            paintBallsLoaded--;
+             paintBalls--;
+            return true;
+        }
+    }
+}
---
+++ EncapsulationPaintballGun/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EncapsulationPaintballGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaintballGun gun = new PaintballGun();
+            while (true)
+            {
+                Console.WriteLine($"{gun.PaintBalls} paint balls, {gun.GetPaintBallsLoaded()} loaded");
+                if (gun.IsEmpty()) Console.WriteLine("WARNING: You are out of ammo!!!");
+                Console.WriteLine("Press space bar to shoot, r to reload, + to add ammo and q to quit.");
+                char key = Console.ReadKey(true).KeyChar;
+                Console.WriteLine("You have pressed: " + key);
+                if (key == ' ') Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                else if (key == 'r') gun.Reload();
+                else if (key == '+') gun.PaintBalls += PaintballGun.MAGAZINE_SIZE;
+                else if (key == 'q') return;
+            }
+        }
+    }
+}
